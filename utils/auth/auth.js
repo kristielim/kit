b3694faafa0c52bb181928cda401d89a241e1fc1
Evan Zhong <evanzhong@ucla.edu>
2019-10-22 19:42:26 -0700

    Evan/implement auth (#4)
    * Basic firebase setup
    
    * save working copy
    
    * moved fb login to homescreen

---
+++ utils/auth/auth.js
@@ -0,0 +1,26 @@
+import firebase from "../firebase/firebase";
+import { FACEBOOK_APP_ID } from "react-native-dotenv";
+import * as Facebook from 'expo-facebook';
+
+export default async function logIn() {
+  try {
+    const {
+      type,
+      token,
+      expires,
+      permissions,
+      declinedPermissions,
+    } = await Facebook.logInWithReadPermissionsAsync(FACEBOOK_APP_ID, {
+      permissions: ['public_profile'],
+    });
+    if (type === 'success') {
+      // Get the user's name using Facebook's Graph API
+      const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+      alert('Logged in!', `Hi ${(await response.json()).name}!`);
+    } else {
+      // type === 'cancel'
+    }
+  } catch ({ message }) {
+    alert(`Facebook Login Error: ${message}`);
+  }
+}
---
+++ utils/firebase/firebase.js
@@ -0,0 +1,24 @@
+import * as firebase from "firebase";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  MESSAGE_SENDER_ID,
+  APP_ID
+} from "react-native-dotenv";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: "",
+  messagingSenderId: MESSAGE_SENDER_ID,
+  appId: APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
