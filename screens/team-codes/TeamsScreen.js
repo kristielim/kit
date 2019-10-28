cd6064d63e281a6c379bc6f106c869f60d431d54
evazhog <evanzhong@ucla.edu>
2019-10-27 19:43:07 -0700

    added new screen, and navigation link

---
+++ screens/team-codes/TeamsScreen.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default function TeamsScreen() {
+  return (
+    <View>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}>
+        <View style={styles.header}>
+          <Text>{/*EVAN TODO: iso this header view, reused on all screens*/}</Text>
+          <Text style={styles.logoText}>KIT</Text>
+          <Image
+            source={
+              __DEV__
+                ? require('../../assets/images/robot-dev.png')
+                : require('../../assets/images/robot-prod.png')
+            }
+            style={styles.logo}
+          />
+        </View>
+
+        <View>
+          <Text>Add a new team</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  logoText: {
+    fontFamily: 'space-mono', //EVAN TODO: Design hand-off
+    fontSize: 35,
+    // lineHeight: 24,
+  },
+  logo: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+});
