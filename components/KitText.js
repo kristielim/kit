eae44e05bf692a77449ee65c0cc105bcebaa1518
evazhog <evanzhong@ucla.edu>
2019-11-06 19:39:49 -0800

    import poligon, saving before reorganizing

--- components/KitText.js
+++ components/KitText.js
@@ -8,42 +8,104 @@ props expected
   style
   color
   fontWeight
+    thin  
+    light
+    regular
+    medium
+    semibold
+    bold
+    extrabold
+    black
+  fontCaligraphy
+    italic
+    link
+    none
   size
 */
 export default function KitText(props) {
   const [fontLoaded, setFontLoaded] = useState("");
+  const [fontToUse, setFontToUse] = useState("");
+
   const defaultFontStyles = {marginTop: "auto", marginBottom: "auto", textAlign: "center"}; //EVAN TODO: move this to constants
 
   const loadFontAssets = async () => {
     await Font.loadAsync({
       // EVAN TODO: move the requires to constants
-      'hind-regular': require('../assets/fonts/Hind/Hind-Regular.ttf'),
-      'hind-bold': require('../assets/fonts/Hind/Hind-Bold.ttf'),
-      'hind-light': require('../assets/fonts/Hind/Hind-Light.ttf'),
-      'hind-medium': require('../assets/fonts/Hind/Hind-Medium.ttf'),
-      'hind-semibold': require('../assets/fonts/Hind/Hind-SemiBold.ttf'),
+      'poligon-black-italic': require('../assets/fonts/Poligon/poligon-black-italic.otf'),
+      'poligon-black-link': require('../assets/fonts/Poligon/poligon-black-link.otf'),
+      'poligon-black': require('../assets/fonts/Poligon/poligon-black.otf'),
+      'poligon-bold-italic': require('../assets/fonts/Poligon/poligon-bold-italic.otf'),
+      'poligon-bold-link': require('../assets/fonts/Poligon/poligon-bold-link.otf'),
+      'poligon-bold': require('../assets/fonts/Poligon/poligon-bold.otf'),
+      'poligon-extrabold-italic': require('../assets/fonts/Poligon/poligon-extrabold-italic.otf'),
+      'poligon-extrabold-link': require('../assets/fonts/Poligon/poligon-extrabold-link.otf'),
+      'poligon-extrabold': require('../assets/fonts/Poligon/poligon-extrabold.otf'),
+      'poligon-light-italic': require('../assets/fonts/Poligon/poligon-light-italic.otf'),
+      'poligon-light-link': require('../assets/fonts/Poligon/poligon-light-link.otf'),
+      'poligon-light': require('../assets/fonts/Poligon/poligon-light.otf'),
+      'poligon-medium-italic': require('../assets/fonts/Poligon/poligon-medium-italic.otf'),
+      'poligon-medium-link': require('../assets/fonts/Poligon/poligon-medium-link.otf'),
+      'poligon-medium': require('../assets/fonts/Poligon/poligon-medium.otf'),
+      'poligon-regular-italic': require('../assets/fonts/Poligon/poligon-regular-italic.otf'),
+      'poligon-regular-link': require('../assets/fonts/Poligon/poligon-regular-link.otf'),
+      'poligon-regular': require('../assets/fonts/Poligon/poligon-regular.otf'),
+      'poligon-semibold-italic': require('../assets/fonts/Poligon/poligon-semibold-italic.otf'),
+      'poligon-semibold-link': require('../assets/fonts/Poligon/poligon-semibold-link.otf'),
+      'poligon-semibold': require('../assets/fonts/Poligon/poligon-semibold.otf'),
+      'poligon-thin-italic': require('../assets/fonts/Poligon/poligon-thin-italic.otf'),
+      'poligon-thin-link': require('../assets/fonts/Poligon/poligon-thin-link.otf'),
+      'poligon-thin': require('../assets/fonts/Poligon/poligon-thin.otf'),
     });
-    setFontLoaded("hind-");
+    setFontLoaded("poligon");
   }
 
-  const pickFontWeight = (weightPassed) => {
-    switch (weightPassed) {
-      case "bold":
-        return fontLoaded + "bold"
+  const pickFontWeight = () => {
+    switch (props.fontWeight) {
+      case "thin":
+        setFontToUse("poligon" + "-thin")
+        break;
       case "light":
-        return fontLoaded + "light"
-      case "medium":
-        return fontLoaded + "medium"
-      case "semibold":
-        return fontLoaded + "semibold"
+        setFontToUse("poligon" + "-light")
+        break;
       default:
-        return fontLoaded + "regular"
+        setFontToUse("poligon" + "-regular")
+        break;
+      case "medium":
+        setFontToUse("poligon" + "-medium")
+        break;
+      case "semibold":
+        setFontToUse("poligon" + "-semibold")
+        break;
+      case "bold":
+        setFontToUse("poligon" + "-bold")
+        break;
+      case "extrabold":
+        setFontToUse("poligon" + "-extrabold")
+        break;
+      case "black":
+        setFontToUse("poligon" + "-black")
+        break;
     }
   }
-  const fontToUse = pickFontWeight(props.fontWeight)
+
+  const pickFontCaligraphy = () => {
+    switch (props.fontCaligraphy) {
+      case "italic":
+        setFontToUse(fontToUse + "-italic")
+        break
+      case "link":
+        setFontToUse(fontToUse + "-italic")
+        break
+      default:
+        break
+    }
+  }
+
 
   useEffect(() => {
     loadFontAssets()
+    pickFontWeight()
+    pickFontCaligraphy()
   }, []);
 
   return (fontLoaded ? (
@@ -52,5 +114,6 @@ export default function KitText(props) {
                                                     color: props.color ? props.color : "black"}]}>
       {props.children}
     </Text>) : null
+    
   );
 }
--- components/team-codes/MyTeams.js
+++ components/team-codes/MyTeams.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Image, Text, View, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 
 import { getTeamsForUserId } from '../../utils/db/handle'
+import KitText from '../KitText';
 
 const _ = require("lodash");
 const myUserId = 'user_id_1'; //EVAN TODO. This is hardcoded for now, just to see if linkup to firebase will work. We can use var userId = firebase.auth().currentUser.uid; later
@@ -13,14 +14,11 @@ export function MyTeams(props) {
 
   return (
     <View style={{marginTop: 10,}}>
-      {myTeams.length == 0 && <Text style={styles.text}>No Teams Yet</Text>}
+      {myTeams.length == 0 && <KitText style={{}} size={36}>No Teams Yet</KitText>}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    textAlign: 'center',
-    fontSize: 36,
-  },
+  
 });
