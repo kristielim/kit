5ccf7c52add6446daf80a9deebbe55085e684423
Kristie Lim <kristie.lim@gmail.com>
2019-11-10 23:40:46 -0800

    Random challenge

--- utils/challenges/challenges.js
+++ utils/challenges/challenges.js
@@ -1,7 +1,15 @@
 import firebase from "../firebase/firebase";
 
 export async function setCurrentChallenge(teamId) {
-  const challengeId = 1; // TODO: change to random number from 0 to challenges.length
+  // pick a random challenge
+  const challengeCountSnapshot = await firebase
+    .database()
+    .ref("/challenges/count")
+    .once("value");
+  const challengeCount = challengeCountSnapshot.val();
+  // challenge ids are indexed at 0
+  const challengeId = Math.floor(Math.random() * challengeCount);
+
   const time = firebase.database.ServerValue.TIMESTAMP;
   // time since the Unix epoch, in milliseconds) as determined by the Firebase servers
   // see https://firebase.google.com/docs/reference/js/firebase.database.ServerValue
