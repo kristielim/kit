d7166c94a1410db9320bc4e9177ee11b8b2c8fb9
Kristie Lim <kristie.lim@gmail.com>
2019-11-21 23:07:11 -0800

    Refactor with background screen

--- components/KitBackgroundScreen.js
+++ components/KitBackgroundScreen.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image, TouchableOpacity } from "react-native";
 import Colors from "../constants/Colors";
 
 export default function KitBackgroundScreen(props) {
   return (
     <View style={styles.background}>
-      <View style={styles.main}>{props.children}</View>
+      <View style={styles.main}>
+        <TouchableOpacity onPress={props.onPressBack}>
+          <Image
+            style={styles.backButton}
+            source={require("../assets/images/arrow.png")}
+          />
+        </TouchableOpacity>
+        {props.children}
+      </View>
     </View>
   );
 }
@@ -19,6 +27,13 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
     borderRadius: 30,
-    backgroundColor: Colors.KIT_WHITE
+    backgroundColor: Colors.KIT_WHITE,
+    padding: 16
+  },
+  backButton: {
+    position: "absolute",
+    top: 16,
+    left: 16,
+    zIndex: 100
   }
 });
--- components/team-codes/Join.js
+++ components/team-codes/Join.js
@@ -12,6 +12,7 @@ import KitText from "../KitText";
 import KitButton from "../KitButton";
 import Colors from "../../constants/Colors";
 import FontStyles from "../../constants/FontStyles";
+import KitBackgroundScreen from "../KitBackgroundScreen";
 
 export function Join(props) {
   const [textValue, setTextValue] = useState("");
@@ -19,51 +20,79 @@ export function Join(props) {
   const [teamFound, setTeamFound] = useState(false);
 
   return (
-    <View>
-      <TouchableOpacity onPress={props.back}>
-        <Text style={{ color: "#E17327", fontSize: 20, textAlign: "center" }}>
-          Back
-        </Text>
-      </TouchableOpacity>
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        onChangeText={async text => {
-          setTextValue(text);
-          if (text.length == 6) {
-            const teamId = await getTeamIdFromTeamCode(text);
-            if (teamId) {
-              const team = await getTeam(teamId);
-              setTeamName(team.name);
-              setTeamFound(true);
-            } else {
-              setTeamFound(false);
+    <KitBackgroundScreen onPressBack={props.back}>
+      <View style={styles.container}>
+        <KitText>Enter Code:</KitText>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={async text => {
+            setTextValue(text);
+            if (text.length == 6) {
+              const teamId = await getTeamIdFromTeamCode(text);
+              if (teamId) {
+                const team = await getTeam(teamId);
+                setTeamName(team.name);
+                setTeamFound(true);
+              } else {
+                setTeamFound(false);
+              }
             }
-          }
-        }}
-        value={textValue}
-      />
-      {textValue.length >= 6 &&
-        (teamFound ? (
-          <>
-            <KitText>Do you want to join {teamName}?</KitText>
-            <TouchableOpacity
-              onPress={() => {
-                joinTeam(textValue, "TODO: real user").then(() => {
-                  setTextValue("");
-                  setTeamFound(false);
-                });
-              }}
-            >
-              <Text
-                style={{ color: "#E17327", fontSize: 20, textAlign: "center" }}
+          }}
+          value={textValue}
+        />
+        {textValue.length >= 6 &&
+          (teamFound ? (
+            <>
+              <KitText>Do you want to join {teamName}?</KitText>
+              <TouchableOpacity
+                onPress={() => {
+                  joinTeam(textValue, "TODO: real user").then(() => {
+                    setTextValue("");
+                    setTeamFound(false);
+                  });
+                }}
               >
-                Join
-              </Text>
-            </TouchableOpacity>
-          </>
-        ) : (
-          <KitText>Team not found</KitText>
-        ))}
-    </View>
+                <Text
+                  style={{
+                    color: "#E17327",
+                    fontSize: 20,
+                    textAlign: "center"
+                  }}
+                >
+                  Join
+                </Text>
+              </TouchableOpacity>
+            </>
+          ) : (
+            <KitText>Team not found</KitText>
+          ))}
+      </View>
+    </KitBackgroundScreen>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: Colors.KIT_GREEN,
+    padding: 20
+  },
+  main: {
+    flex: 1,
+    borderRadius: 30,
+    backgroundColor: Colors.KIT_WHITE,
+    padding: 16
+  },
+  backButton: {
+    position: "absolute",
+    top: 16,
+    left: 16,
+    zIndex: 100
+  },
+  container: {
+    display: "flex"
+  },
+  textInput: {
+    margin: "20% 0%"
+  }
+});
