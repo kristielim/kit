4d2988b13f2a3efcc16b06e3fa0415d4c7784d25
Kristie Lim <kristie.lim@gmail.com>
2019-11-27 03:29:22 -0800

    Sign in screen

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Input } from "../components/input";
-import { Button } from "../components/button";
-import { signIn } from "../utils/auth/auth";
+import { StyleSheet, Image, View } from "react-native";
+import KitText from "../components/KitText";
+import KitTextInput from "../components/KitTextInput";
+import KitButtonSupreme from "../components/KitButtonSupreme";
+import { signUp, checkSignUpEmail, signIn } from "../utils/auth/auth";
+import Colors from "../constants/Colors";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function SignIn(props) {
   const { navigation } = props;
@@ -13,54 +16,146 @@ export default function SignIn(props) {
   };
 
   return (
-    <View>
-      <Input
-        label="Email Address"
-        placeholder="you@domain.com"
-        onChangeText={setEmail}
-        value={email}
-      />
-      <Input
-        label="Password"
-        autoCorrect={false}
-        placeholder="*******"
-        secureTextEntry
-        onChangeText={setPassword}
-        value={password}
-      />
-      <Button
-        onPress={() => {
-          signIn(email, password);
-        }}
-      >
-        Sign In{" "}
-      </Button>
-      <Button onPress={goToSignUp}>Don't have an account? Sign up </Button>
+    <View style={styles.background}>
+      <View style={styles.main}>
+        <View>
+          <View style={styles.logoContainer}>
+            <Image
+              style={styles.logo}
+              source={require("../assets/images/logo.png")}
+            />
+            <Image
+              style={styles.logoText}
+              source={require("../assets/images/wordmark.png")}
+            />
+            {/* Chose not to use because of padding issue
+          <KitText
+            style={styles.logoText}
+            color={Colors.KIT_BLACK}
+            fontWeight="bold"
+            size={100}
+          >
+            kit
+          </KitText> */}
+          </View>
+          <KitText color={Colors.KIT_DARK_GREY} fontWeight="semibold" size={20}>
+            keep in touch
+          </KitText>
+        </View>
+        <View style={styles.textContainer}>
+          <KitText
+            style={styles.text}
+            color={Colors.KIT_BLACK}
+            size={32}
+            fontWeight="bold"
+          >
+            Welcome back!
+          </KitText>
+          <KitText
+            style={styles.text}
+            color={Colors.KIT_LIGHT_ORANGE}
+            size={16}
+            fontWeight="semibold"
+          >
+            Please sign in below.
+          </KitText>
+        </View>
+        <View style={styles.textInputContainer}>
+          <View>
+            <KitTextInput
+              image={require("../assets/images/onboardingemail.png")}
+              placeholder="Email"
+              onChangeText={setEmail}
+              value={email}
+            />
+            <KitTextInput
+              image={require("../assets/images/onboardingpassword.png")}
+              placeholder="Password"
+              onChangeText={setPassword}
+              value={password}
+              secureTextEntry
+            />
+          </View>
+          <KitButtonSupreme
+            onPress={() => {
+              signIn(email, password);
+            }}
+            color={Colors.KIT_LIGHT_ORANGE}
+            width={233}
+          >
+            SIGN IN
+          </KitButtonSupreme>
+          <TouchableOpacity onPress={goToSignUp}>
+            <KitText color={Colors.KIT_RED} fontWeight="semibold" size={16}>
+              Forgot Password?
+            </KitText>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.bottomText}>
+          <KitText color={Colors.KIT_DARK_GREY} fontWeight="medium" size={16}>
+            Don't have an account?{" "}
+          </KitText>
+          <TouchableOpacity onPress={goToSignUp}>
+            <KitText
+              color={Colors.KIT_LIGHT_ORANGE}
+              fontWeight="semibold"
+              size={16}
+            >
+              Sign up
+            </KitText>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
+  background: {
+    backgroundColor: Colors.KIT_LIGHT_GREY,
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%"
+  },
+  bottomText: {
+    flexDirection: "row",
+    width: 268,
+    justifyContent: "space-around",
+    margin: 22
+  },
+  logo: {
+    height: 66,
+    width: 66,
+    margin: 8
+  },
+  logoContainer: {
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center"
+  },
+  logoText: {
+    margin: 8
+  },
+  main: {
+    alignItems: "center"
+  },
+  text: {
+    textAlign: "left"
+  },
+  textContainer: {
+    margin: 22,
+    width: 268
+  },
+  textInputContainer: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    height: 340,
+    width: 300,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    padding: 16
   }
 });
-
-// import { StyleSheet, Text, View, Button } from "react-native";
-
-// export default class SignIn extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Login</Text>
-//         <Button
-//           title="Go to Signup"
-//           onPress={() => this.props.navigation.navigate("Signup")}
-//         />
-//       </View>
-//     );
-//   }
-// }
