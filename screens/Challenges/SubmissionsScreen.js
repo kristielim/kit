89f43594ead202d0d1032ab8ce6bf687485642a7
biancavillamor <56615484+biancavillamor@users.noreply.github.com>
2019-12-04 02:07:06 -0800

    Bianca/challengecards (#40)
    * Bianca/KitButtonSupreme fixed bugs to make kit button self-contained, has image option for icons, only bug is weird off padding from kit text
    
    * added icons for individual challenges, made individual challenge screen, still doesn't connect to other screens but i tested on profile,
    added padding options on KitBackgroundScreen through props to fix weird padding issues when making individual challenge screen - username MUST be present!
    also fixed weird padding issues in challengecard and revealchallenge
    WIP: fixing italic text issue, adding responses thread, connecting to other screens
    
    * - made changes to variable name of padding prop to prop
    - added margin to image container as evan suggested

--- screens/Challenges/SubmissionsScreen.js
+++ screens/Challenges/SubmissionsScreen.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import KitText from "../../components/KitText";
 
 export default function SubmissionsScreen() {
   return (
-    <View>
+    <ScrollView>
       <KitText style={styles.label} fontWeight="medium" size={24}>
         Today
       </KitText>
       <KitText style={styles.label} fontWeight="medium" size={24}>
         Yesterday
       </KitText>
-    </View>
+    </ScrollView>
   );
 }
 
--- screens/Challenges/ToDoScreen.js
+++ screens/Challenges/ToDoScreen.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import KitText from "../../components/KitText";
+import ChallengesScreen from "../ChallengesScreen";
+import Colors from "../../constants/Colors";
+import { ScrollView } from "react-native";
 
 export default function ToDoScreen() {
   return (
-    <View>
-      <KitText style={styles.label} fontWeight="medium" size={24}>
-        To Do
-      </KitText>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <ChallengesScreen />
+        <KitText style={styles.label} fontWeight="medium" size={24}>
+          To Do
+        </KitText>
+        <KitText>add challenge cards here yaga yee haw</KitText>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  label: { margin: 36, textAlign: "left" }
+  label: { margin: 36, textAlign: "left" },
+  container: {
+    paddingTop: 10,
+    flex: 1,
+    backgroundColor: Colors.KIT_WHITE
+  }
 });
