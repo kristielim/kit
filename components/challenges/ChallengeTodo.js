74651f41da4f75e69b6dc9f75dc54e44865ca90b
evazhog <evanzhong@ucla.edu>
2019-12-04 20:16:34 -0800

    component-ified challenge todo

---
+++ components/challenges/ChallengeTodo.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Image } from "react-native";
+import Colors from "../../constants/Colors";
+import KitText from "../KitText";
+
+export default function ChallengeTodo(props){
+  return (
+    <View style={{backgroundColor: Colors.KIT_LIGHT_ORANGE, alignItems: "center"}}>  
+      <View style={{flexDirection: "row", alignItems: "center", borderRadius: 10, backgroundColor: Colors.KIT_WHITE, margin: 15, padding: 15, paddingLeft: 30, paddingRight: 30,}}>
+        <View style={{flex: 3, flexDirection: "column"}}>
+          <View style={{flexDirection: "row"}}>
+            <Image style={{maxWidth: 25, maxHeight: 25,}} source={require("../../assets/images/specificTeamsPage/bunny.png")}/>
+            <KitText size={20} color={Colors.KIT_DARKEST_BLACK} fontWeight={"extrabold"}>APUSH HOES</KitText>
+          </View>
+
+          <View style={{marginTop: 5}}>
+            <KitText style={{textAlign: "left"}} size={12} color={Colors.KIT_DARKEST_BLACK} fontWeight={"semibold"}>Challenge: Self Portrait</KitText>
+            <KitText style={{textAlign: "left"}} size={12} color={Colors.KIT_DARKEST_BLACK}>Self Portraits Let out your inner DaVince and make a portrait of your partner! #MonaLisaWho?</KitText>
+          </View>
+
+          <View>
+            {/* The progress bar goes here */}
+          </View>
+        </View>
+        <View style={{flex: 2, alignItems: "flex-end"}}>
+          {/* This is the timer component */}
+          <Image style={{maxWidth: 95, maxHeight: 95,}} source={require("../../assets/images/timer-placeholder.png")}/>
+        </View>
+      </View>
+    </View>
+  );
+}
--- screens/Challenges/ToDoScreen.js
+++ screens/Challenges/ToDoScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { View, StyleSheet, Image } from "react-native";
+import { ScrollView } from "react-native";
+
 import KitText from "../../components/KitText";
 import ChallengesScreen from "../ChallengesScreen";
+import ChallengeTodo from "../../components/challenges/ChallengeTodo";
+
 import Colors from "../../constants/Colors";
 import Fonts from "../../constants/Fonts";
-import { ScrollView } from "react-native";
 
 export default function ToDoScreen() {
   return (
@@ -14,29 +17,7 @@ export default function ToDoScreen() {
         <KitText style={styles.label} fontWeight="medium" size={24}>
           To Do
         </KitText>
-        <View style={{flexDirection: "row"}}>
-          <View style={{flexDirection: "column"}}>
-            <View style={{flexDirection: "row"}}>
-              <Image style={{maxWidth: 25, maxHeight: 25,}} source={require("../../assets/images/specificTeamsPage/bunny.png")}/>
-              <KitText size={20} color={Colors.KIT_DARKEST_BLACK} fontWeight={Fonts.BOLD}>APUSH HOES</KitText>
-            </View>
-
-            <View>
-              <KitText style={{textAlign: "left"}}>Challenge: Self Portrait</KitText>
-              <KitText style={{textAlign: "left"}}>Self Portraits Let out your inner DaVince and make a portrait of your partner! #MonaLisaWho?</KitText>
-            </View>
-
-            <View>
-              
-            </View>
-          </View>
-
-          <View>
-            {/* This is the timer component */}
-            <Image style={{maxWidth: 95, maxHeight: 95,}} source={require("../../assets/images/timer-placeholder.png")}/>
-          </View>
-        </View>
-        <KitText>add challenge cards here yaga yee haw</KitText>
+        <ChallengeTodo/>
       </View>
     </ScrollView>
   );
