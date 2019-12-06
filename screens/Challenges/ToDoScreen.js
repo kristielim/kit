c5df965b06c012813417ff4d9406170c30792db9
evazhog <evanzhong@ucla.edu>
2019-12-06 15:58:16 -0800

    cleaner rendering with switch

--- screens/Challenges/ToDoScreen.js
+++ screens/Challenges/ToDoScreen.js
@@ -20,15 +20,22 @@ export default function ToDoScreen(props) {
     let todos = []
     let alternator = true;
     for (challenge of challenges) {
+      console.log(challenge.assignedChallengeId)
       if(challenge.submissions && challenge.submissions[currentUser]) continue; //If the user has a submission for this challenge, filter it out from Todo
+      
+      let screentoNav;
+      if(challenge.challengeDetails.mediaType === "STRING"){
+        screentoNav = "UploadText";
+      }
+      else if(challenge.challengeDetails.mediaType === "IMAGE"){
+        screentoNav = "UploadImage";
+      }
+      else{
+        //Do some error handling
+      }
       todos.push(
-        <ChallengeTodo key={challenge.teamId} challenge={challenge} mainColor={(alternator ? Colors.KIT_LIGHT_ORANGE : Colors.KIT_GREEN)} onPress={() => {
-          if(challenge.challengeDetails.mediaType === "STRING"){
-            props.navigation.navigate("UploadText", {challenge})
-          }
-          else if(challenge.challengeDetails.mediaType === "IMAGE"){
-            props.navigation.navigate("UploadImage", {challenge})
-          }
+        <ChallengeTodo key={challenge.assignedChallengeId} challenge={challenge} mainColor={(alternator ? Colors.KIT_LIGHT_ORANGE : Colors.KIT_GREEN)} onPress={() => {
+          props.navigation.navigate(screentoNav, {challenge})
         }}/>
       )
       alternator = !alternator;
