eb6c9e4c576629ce4809e0f841b584ff426aa780
Kristie Lim <kristie.lim@gmail.com>
2020-12-30 14:54:22 -0800

    Bug in profile

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -37,9 +37,7 @@ export default function ProfileScreen() {
     getUsername(currentUser).then(name => {
       setName(name);
     });
-  }, []);
 
-  useEffect(() => {
     Permissions.askAsync(Permissions.CAMERA_ROLL);
     console.log("current", currentUser);
     getPicture(currentUser).then(image => {
